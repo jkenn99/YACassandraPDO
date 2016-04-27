1950a24d71b02391eb6fc301abc4364cf3d549e7
Jin Hu <bixuehujin@gmail.com>
2016-04-27 23:04:07 +0800

    Added travis-ci support

---
+++ travis-ci/install-dep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm -rf /var/lib/cassandra/*
+wget http://www.us.apache.org/dist/cassandra/2.1.14/apache-cassandra-2.1.14-bin.tar.gz && tar -xvzf apache-cassandra-2.1.14-bin.tar.gz && sudo sh apache-cassandra-2.1.14/bin/cassandra
+wget https://github.com/apache/thrift/archive/0.9.1.tar.gz
+tar xf 0.9.1.tar.gz
+cd thrift-0.9.1 && ./bootstrap.sh && ./configure --without-php --without-java --without-ruby --without-erlang --without-nodejs --without-lua --without-go --without-python --with-cpp --without-c_glib && make && sudo make install
